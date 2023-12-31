bd7bb43aff395795e4e68d1666558f5eae69a116
Ashur Cabrera <hello@ashur.cab>
2023-12-30 18:15:31 -0800

    Replace fetch with eleventy-fetch

--- eleventy.plugin.js
+++ eleventy.plugin.js
@@ -4,14 +4,17 @@ const { fetchRemoteData } = require( "./src/fetchRemoteData" );
 /**
  * @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
  * @param {Object} options
+ * @param {Object} [options.cache] - options passed to eleventy-fetch
  * @param {Object} options.data
  */
 module.exports = ( eleventyConfig, options ) =>
 {
+	// See https://www.11ty.dev/docs/plugins/fetch/#options
+	const cacheOptions = options.cache || {};
 	const data = options.data || {};
 
 	Object.entries( data ).forEach( ( [key, url] ) =>
 	{
-		eleventyConfig.addGlobalData( key, async () => await fetchRemoteData( url ) );
+		eleventyConfig.addGlobalData( key, async () => await fetchRemoteData( url, cacheOptions ) );
 	} );
 };
--- src/fetchRemoteData.js
+++ src/fetchRemoteData.js
@@ -1,30 +1,24 @@
+const eleventyFetch = require( "@11ty/eleventy-fetch" );
+
 /**
  * @param {string} remoteUrl
+ * @param {Object} cacheOptions
  * @return {Array|Object}
  */
-module.exports.fetchRemoteData = async( remoteUrl ) =>
+module.exports.fetchRemoteData = async( remoteUrl, cacheOptions ) =>
 {
+	cacheOptions.type = "json";
+
 	try
 	{
 		const url = new URL( remoteUrl );
-		const response = await fetch( url, {
-			headers: {
-				Accept: "application/json",
-			},
-		} );
-
-		return await response.json();
+		return await eleventyFetch( url.toString(), cacheOptions );
 	}
 	catch( error )
 	{
 		if( error.code === "ERR_INVALID_URL" )
 		{
 			throw new Error( "Missing or invalid url: " + remoteUrl );
-		}
-
-		if( error.constructor?.name === "SyntaxError" )
-		{
-			throw new Error( "Invalid JSON response received: " + remoteUrl );
 		}
 
 		throw error;
